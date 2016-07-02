a2f0e960a2b0be9a5c6231b9d6fbb1729fb04e50
Valeriy Palchenko <alpalch@gmail.com>
2016-07-02 22:45:25 +0300

    add logo

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates_presence_of :content, presence: true
-  has_attached_file :photo, :styles => { :small => "150x150>" },
+  has_attached_file :photo, :styles => { :small => "180x180>" },
     :url => "/assets/posts/:id/:style/:basename.:extension",
     :path => ":rails_root/public/assets/posts/:id/:style/:basename.:extension"
   validates_attachment_size :photo, :less_than => 5.megabytes
